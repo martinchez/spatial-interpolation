7cf97310906e1fa288b34122f204e44e8705574e
martinchez <martinchege1998@gmail.com>
2020-07-07 16:09:58 +0300

    updating kriging to test IDW

--- ordinary kriging.R
+++ ordinary kriging.R
@@ -8,3 +8,10 @@ ok<- brick(kp)
 ok<- mask(ok,ca)
 names(ok)<-c('prediction','variance')
 plot(ok)
+
+#compare with other methodes
+library(gstat)
+idm<- gstat(formula =OZDLYAV~1, locations=aq )
+idp<-interpolate(r,idm)
+idp<-mask(idp,ca)
+plot(idp)
